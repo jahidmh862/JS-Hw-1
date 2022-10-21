dec7a178da77c0bfafeb6ebb633a7cfa3cdbbf9d
jahidmh862 <112058851+jahidmh862@users.noreply.github.com>
2022-10-21 12:52:35 +0600

    Add files via upload

---
+++ index.html/js/app.js
@@ -0,0 +1,44 @@
+function marksheet (){
+
+    var num = document.getElementById('num').value
+
+    var result = document.getElementById('result')
+    
+if(num >= 80 && num <= 100){
+    result.innerText = ' I Am A+';
+}
+else if(num >= 70 && num <= 79){
+    result.innerText = 'A';
+}
+else if(num >= 60 && num <= 69){
+    result.innerText = 'A-';
+}
+else if(num >= 50 && num <= 59){
+   result.innerText = 'B';
+}
+else if(num >= 40 && num <= 49){
+    result.innerText = 'C';
+}
+else if(num >= 30 && num <= 39){
+    result.innerText = 'D';
+}
+else if(num >= 0 && num <= 33){
+   result.innerText = 'FAIL';
+}
+else{
+    alert('failed');
+}
+}
+
+function oddeven(){
+    var user_number = document.getElementById('num').value
+
+    var user_result = document.getElementById('Result');
+
+    if(user_number % 2 == 0){
+        user_result.innerText='Even Number'
+    }
+    else{
+        user_result.innerText='Odd Number' 
+    }
+}
